483c89d63edb83936a6157bfc44c6ac76c9399fe
androidinflow <zakharname0111@gmail.com>
2024-07-09 21:12:27 +0200

    username log2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   export let data;
   $: ({ user, isLoggedIn } = data);
   const { posts } = data;
-  console.log(user?.username);
+  console.log(data.user.email);
 </script>
 
 <h1 class="text-3xl font-bold underline text-red-400">RED RUBY</h1>
