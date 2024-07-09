952b7430cfad52b78ba2211514a088ae21db5e74
androidinflow <zakharname0111@gmail.com>
2024-07-09 20:51:52 +0200

    login nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,21 @@
 <script>
   import "../app.css";
+  export let data;
+  $: ({ user, isLoggedIn } = data);
 </script>
 
 <div class="navbar bg-base-200">
-  <a href="https://kon.redruby.one" class="btn btn-ghost text-xl">RedRuby</a>
+  <div>
+    <a href="https://kon.redruby.one" class="btn btn-ghost text-xl">RedRuby</a>
+    {#if isLoggedIn}
+      <a href="/logout">
+        <button class="btn btn-neutral">Logout</button>
+      </a>
+    {:else}
+      <a href="/login">
+        <button class="btn btn-neutral">Login using Github</button>
+      </a>
+    {/if}
+  </div>
 </div>
 <div class="container mx-auto pt-2"><slot></slot></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,47 +5,44 @@
   console.log(posts);
 </script>
 
-<div class="container mx-auto px-4">
-  <h1 class="text-3xl font-bold underline text-red-400">RED RUBY</h1>
-  <img
-    src="https://gordonmax.com/wp-content/uploads/2020/06/red_emerald.jpg"
-    width="100"
-    height="100"
-    alt="red-ruby"
-  />
+<h1 class="text-3xl font-bold underline text-red-400">RED RUBY</h1>
+<img
+  src="https://gordonmax.com/wp-content/uploads/2020/06/red_emerald.jpg"
+  width="100"
+  height="100"
+  alt="red-ruby"
+/>
 
-  {#if isLoggedIn}
-    <h2>Welcome {user?.username}</h2>
-    <h3>{data.user.email}</h3>
+{#if isLoggedIn}
+  <h2>Welcome {user?.username}</h2>
+  <h3>{data.user.email}</h3>
 
-    <a href="/logout">
-      <button class="btn btn-neutral">Logout</button>
-    </a>
-  {:else}
-    <a href="/login">
-      <button class="btn btn-neutral">Login using Github</button>
-    </a>
-  {/if}
+  <a href="/logout">
+    <button class="btn btn-neutral">Logout</button>
+  </a>
+{:else}
+  <a href="/login">
+    <button class="btn btn-neutral">Login using Github</button>
+  </a>
+{/if}
 
-  <h1>Posts</h1>
-
-  {#if posts.length > 0}
-    <ul>
-      {#each posts as post}
-        <li>
-          <h2>Post ID: {post.id}</h2>
-          <p>{post.text}</p>
-          {#if post.image}
-            <img
-              width="400px"
-              src="https://end.redruby.one/api/files/posts/{post.id}/{post.image}"
-              alt={posts.Text}
-            />
-          {/if}
-        </li>
-      {/each}
-    </ul>
-  {:else}
-    <p>No posts available.</p>
-  {/if}
-</div>
+{#if posts.length > 0}
+  <ul>
+    {#each posts as post}
+      <h1>Posts</h1>
+      <li>
+        <h2>Post ID: {post.id}</h2>
+        <p>{post.text}</p>
+        {#if post.image}
+          <img
+            width="400px"
+            src="https://end.redruby.one/api/files/posts/{post.id}/{post.image}"
+            alt={posts.Text}
+          />
+        {/if}
+      </li>
+    {/each}
+  </ul>
+{:else}
+  <p>No posts available. maybe if u log in?</p>
+{/if}
