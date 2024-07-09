62cd933b5cd1b880dd42f792d5117b22bb19307b
androidinflow <zakharname0111@gmail.com>
2024-07-09 21:41:50 +0200

    img nav style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,17 @@
 </script>
 
 <div class="navbar bg-base-200">
-  <div class="flex justify-between w-full">
-    <a href="https://kon.redruby.one" class="btn btn-ghost text-xl">RedRuby</a>
-    {#if isLoggedIn}
-      <div class="avatar">
-        <div class="mask mask-squircle w-24">
-          <img src={"https://joesch.moe/api/v1/" + user?.username} alt="kos" />
-        </div>
+  <div class="flex justify-between w-full mx-1">
+    <a href="https://kon.redruby.one" class="btn btn-ghost text-xl m-0 p-0"
+      >RedRuby</a
+    >
+    <div class="avatar">
+      <div
+        class="ring-black ring-offset-black w-10 rounded-full ring ring-offset-1"
+      >
+        <img src="https://joesch.moe/api/v1/jess" alt="kos" />
       </div>
-    {/if}
+    </div>
   </div>
 </div>
 <div class="container mx-auto pt-2"><slot></slot></div>
