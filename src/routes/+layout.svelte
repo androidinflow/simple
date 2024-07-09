a76e2ec34e0673a3169cb0a3caf5d3253dd90055
androidinflow <zakharname0111@gmail.com>
2024-07-09 20:45:29 +0200

    logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class="navbar bg-base-200">
-  <a href="kon.redruby.one" class="btn btn-ghost text-xl">daisyUI</a>
+  <a href="https://kon.redruby.one" class="btn btn-ghost text-xl">RedRuby</a>
 </div>
 <div class="container mx-auto pt-2"><slot></slot></div>
