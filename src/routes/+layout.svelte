5f26824d6b6f83b5511f49f507dec2c3f28e1a4d
androidinflow <zakharname0111@gmail.com>
2024-07-09 19:37:12 +0200

    margin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,4 @@
 <div class="navbar bg-base-200">
   <a href="kon.redruby.one" class="btn btn-ghost text-xl">daisyUI</a>
 </div>
-
-<slot></slot>
+<div class="container mx-auto px-4 pt-2"><slot></slot></div>
